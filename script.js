39939a00b00e0a1a8599c134cca3c922a8efce71
Alexander <alex.savvutin@gmail.com>
2024-08-09 15:30:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function handleImageClick(imageId) {
             selectedWallID = i
             imgURL = wallImageURLs[i]
         }
-    //document.getElementById("svgDivID").style.backgroundImage = `url(${imgURL})`/*${selectedImage.src}urls[id]*/
+    img.src = imgURL+document.getElementById("myCanvas").width //document.getElementById("svgDivID").style.backgroundImage = `url(${imgURL})`/*${selectedImage.src}urls[id]*/
     refreshScreen()
 }
 
@@ -66,7 +66,8 @@ function handleFileSelect() {
             // Add the new image to the bottom bar
             const bottomBar = document.querySelector('.bottom-bar');
             bottomBar.insertBefore(newImage, bottomBar.lastElementChild); // Insert before the button
-            handleImageClick('imageCustom')
+            img.src = newImage.src
+            refreshScreen()
         };
         reader.readAsDataURL(file);
     }
@@ -88,6 +89,7 @@ function heightSliderTweak(){
     doorHeight = parseInt(rangeHeight.value)
     var numberHeight = document.getElementById("height")
     numberHeight.value = doorHeight
+    coords.h = doorHeight
     refreshScreen()
 }
 
@@ -96,6 +98,7 @@ function heightNumberTweak(){
     var numberHeight = document.getElementById("height")
     doorHeight = parseInt(numberHeight.value)
     rangeHeight.value = doorHeight
+    coords.h = doorHeight
     refreshScreen()
 }
 
@@ -105,6 +108,7 @@ function widthSliderTweak(){
     doorWidth = parseInt(rangeWidth.value)
     var numberWidth = document.getElementById("width")
     numberWidth.value = doorWidth
+    coords.w = doorWidth
     refreshScreen()
 }
 
@@ -113,6 +117,7 @@ function widthNumberTweak(){
     var numberWidth = document.getElementById("width")
     doorWidth = parseInt(numberWidth.value)
     rangeWidth.value = doorWidth
+    coords.w = doorWidth
     refreshScreen()
 }
 
@@ -135,26 +140,32 @@ textureGrid.addEventListener('click', function(event) {
 function drawImageWithRectangle(imageUrl, rectColor, rectSize) {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    //console.log(canvas.width)
+    //console.log('A ' + canvas.width)
+    var div = document.getElementById('svgDivID').getBoundingClientRect()
+    canvas.width = div.width
+    canvas.height = div.height
+    //console.log('B ' + canvas.width)
     // Create an image object
-    var img = new Image();
+/*    var img = new Image();
     img.onload = function() {
         // Draw the image on the canvas
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-        var canvasScaleX = canvas.width/100;
-        var canvasScaleY = canvas.height/100;
+    }*/ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        var canvasScaleX = canvas.width/800;
+        var canvasScaleY = canvas.height/600;/*height*/
         var w = doorWidth * 0.5
         var h = doorHeight * 0.5
         var y = 50
-        var rectX = canvas.width * 0.5 - w * 0.5
-        var rectY = canvas.height * 0.5 - h * 0.5
+        var rectX = canvas.width * 0.5 /** canvasScaleX*/ - coords.w * 0.5 /** canvasScaleX*/
+        var rectY = canvas.height * 0.5 /** canvasScaleY*/ - coords.h * 0.5 /** canvasScaleY*/
         // Draw the rectangle on top of the image
+        ctx.fillStyle = 'white' //brown
+        ctx.fillRect(rectX-3, rectY-3, coords.w+6, coords.h+6)
         ctx.fillStyle = rectColor
         if (selectedColor != "")
-            ctx.fillRect(rectX, 50+rectY, w*0.5, h*0.5)
-        else if (selectedTexture != ""){ console.log(textures[selectedTexture]) /*textures.forEach(item => {})*/
-            ctx.drawImage(textures[selectedTexture], rectX, 50+rectY, w*0.5, h*0.5)}
+            ctx.fillRect(rectX/*+rectX*canvasScaleX*/, /*50+*/rectY/**canvasScaleY*/, coords.w/**canvasScaleX*//*w*0.5*/, coords.h/**canvasScaleY*//*h*0.5*/)
+        else if (selectedTexture != ""){ //console.log(textures[selectedTexture]) /*textures.forEach(item => {})*/
+            ctx.drawImage(textures[selectedTexture], rectX, /*50+*/rectY, coords.w/**0.5*/, coords.h/**0.5*/)}
         //ctx.fillRect(100,/*canvas.width/2 - rectSize/2,*/ 19+(105-doorHeight/5)/*canvas.height/2 - rectSize/2*/, doorWidth/3/**canvasScaleX*/, doorHeight/5/**canvasScaleY*/);
         //drawDoor(5*100/doorWidth, 5*100/doorHeight) //svg()
         // var svgUrl = 'data:image/svg+xml;base64,' + btoa(new XMLSerializer().serializeToString(document.getElementById('svgCanvas')));
@@ -165,24 +176,52 @@ function drawImageWithRectangle(imageUrl, rectColor, rectSize) {
         var drawPeephole = document.getElementById("peephole").checked
         var drawDoorbell = document.getElementById("doorbell").checked
         // var drawPetdoor = document.getElementById("petdoor").checked
-        if(drawLock) ctx.fillRect(rectX+w*0.3, 50+rectY+h*0.3, 5, 5)
-        if(drawHandle) ctx.fillRect(rectX+w*0.3, 50+rectY+h*0.195, 5, 5)
-        if(drawHinges) { ctx.fillRect(rectX+w*0.1-7, 50+rectY+h*0.05, 5, 5) ; ctx.fillRect(rectX+w*0.1-7, 50+rectY+h*0.3, 5, 5) }
-        if(drawPeephole) ctx.fillRect(rectX+w*0.1, 50+rectY+h*0.1, 5, 5)
-        if(drawDoorbell) ctx.fillRect(rectX+w*0.5, 50+rectY+h*0.1, 5, 5)
+        if(drawLock) { feature(rectX+coords.w-30, 50+rectY+coords.h*0.35, 9, 16, ctx) }
+        if(drawHandle) { feature(rectX+coords.w-30/**0.3*/, 50+rectY+coords.h*0.31, 16, 5, ctx) }
+        if(drawHinges) { feature(rectX-6, 50+rectY+coords.h*0.05+3, 16, 16, ctx) ; feature(rectX-6, /*50+*/rectY+coords.h-coords.h*0.3+3, 16, 16, ctx) }
+        if(drawPeephole) { feature(rectX+coords.w*0.5-3, 50+rectY+coords.h*0.1-3, 16, 16, ctx); }
+        if(drawDoorbell) { feature(rectX+coords.w, 50+rectY+coords.h*0.1, 9, 9, ctx) }
         // if(drawPetdoor) ctx.fillRect(rectX+w*0.35, 5+rectY+h*0.8, 16, 16)
         // var imgSVG = new Image();
         // imgSVG.onload = function() {
         // ctx.drawImage(img, 0, 0);
         // };
         // imgSVG.src = svgUrl;
-    };
-    if (selectedWallID != 3)
-        img.src = `${imageUrl}${canvas.width}`
-    else
-        img.src = document.getElementById("imageCustom").src
+        arrow(rectX/*+coords.w*/, rectY/*-coords.h-19*/, coords.w, coords.h, ctx)
+    //if (selectedWallID != 3)
+    //    img.src = `${imageUrl}${canvas.width}`
+    //else
+    //    img.src = document.getElementById("imageCustom").src
 }
-
+function feature(x, y, sizeW, sizeH, ctx) {
+    ctx.fillStyle = "white"
+    ctx.fillRect(x-3, y-3, sizeW+6, sizeH+6)
+    ctx.fillStyle = "green"
+    ctx.fillRect(x, y, sizeW, sizeH)
+}
+function arrow(x, y, w, h, ctx)
+{
+    var a = x/*+ w + 35*/
+    var b = y + (/*50+*/h)
+    var c = /*x + */w /*+ 35*/
+    var d = /*y+*/5 /*270*/
+    ctx.font = "bold 16px serif"
+    ctx.fillRect(a, b-19-h, c, d) ; ctx.fillRect(a-1, b-23-h, 5, 16) ; ctx.fillRect(a+w-1, b-23-h, 5, 16)
+    ctx.fillText(`Width: ${doorWidth} cm`, a+w*0.1, b-19-h-19)
+    ctx.fillRect(a+w+19, b-h, 5, h) ; ctx.fillRect(a+w+13, b-h-1, 16, 5) ; ctx.fillRect(a+w+13, b-1, 16, 5)
+    ctx.fillText(`Height: ${doorHeight} cm`, a+w+35, b-h*0.5-5)
+    // ctx.strokeStyle = "green"
+    // var currLineWidth = ctx.lineWidth
+    // ctx.lineWidth = 3
+    // ctx.beginPath()
+    // ctx.moveTo(a,b)
+    // ctx.lineTo(c,d)
+    // ctx.lineTo(a+5,d+5)
+    // ctx.lineTo(a,b)
+    // ctx.closePath()
+    // ctx.stroke()
+    // ctx.lineWidth = currLineWidth
+}
 // function svg(){
 //     var coords = {x: 10, y:10, w:100, h:100}
 //     var svg = Snap("#svgCanvas")
@@ -245,10 +284,12 @@ function drawImageWithRectangle(imageUrl, rectColor, rectSize) {
 function refreshScreen() {
     drawImageWithRectangle(wallImageURLs[selectedWallID], selectedColor, doorWidth)
 }
+var img = new Image()
 
 // selectTab('step1')
 var wallImageURLs = ["https://img.freepik.com/free-photo/empty-room-gray-wall-room-with-wooden-floor_53876-128781.jpg?w=", "https://img.freepik.com/free-photo/plants-on-a-wooden-floor-in-empty-white-room_41470-4664.jpg?w=", "https://img.freepik.com/free-photo/dark-wall-empty-room-with-plants-on-a-floor-3d-rendering_41470-3847.jpg?w="]
 var wallImageIDs = [ 'image1', 'image2', 'image3', 'imageCustom' ]
+img.src = `${wallImageURLs[0]}${Math.round(document.getElementById('svgDivID').getBoundingClientRect().width)}`
 // Get available screen width and height
 const availWidth = screen.availWidth;
 const availHeight = screen.availHeight;
@@ -260,7 +301,7 @@ var textures = {"texture1.png": new Image("images/texture1.png"), "texture2.png"
 for(var i = 1; i <= 9; i++) { textures[`texture${i}.png`] = new Image(); textures[`texture${i}.png`].src = `images/texture${i}.png`}
    // Initialize Snap.svg with the SVG canvas
     // var s = Snap("#svgCanvas");
-
+    var coords = {x: 100, y: 100, w:90, h: 210}
     // Define door dimensions and position
     var doorWidth = 90;
     var doorHeight = 210;
@@ -271,7 +312,7 @@ for(var i = 1; i <= 9; i++) { textures[`texture${i}.png`] = new Image(); texture
     // rect.attr({
     //     fill: "#00FF00" // Green color
     // });
-    refreshScreen()
+    img.onload = function() { refreshScreen() }
     // // Draw the door panel with texture
     // var doorPanel = s.rect(doorX, doorY, doorWidth, doorHeight).attr({
     //   fill: 'url(#texture)',  // Use a texture pattern for fill
